0d6049a53a3e1f5f2c2e355288a29d1eabbf02ad
Qais Wardag <qais.wardag@outlook.com>
2023-05-26 14:07:47 +0200

    Include JSON file from public folder in JIT!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   mode: 'jit',
   // These paths are just examples, customize them to match your project structure
-  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    './public/**/*.json',
+  ],
   content: [
     './index.html',
     './public/**/*.html',
