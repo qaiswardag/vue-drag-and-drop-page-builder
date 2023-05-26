19909c09a139532e04ccf92186fe7108a64a4420
Qais Wardag <qais.wardag@outlook.com>
2023-05-26 14:01:08 +0200

    Enable Tailwind JIT mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
+  mode: 'jit',
+  // These paths are just examples, customize them to match your project structure
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   content: [
     './index.html',
     './public/**/*.html',
